fc5872cd738760926a0d73c3536cbe0360721bc5
jeflucas <jef.lucas@hotmail.co.uk>
2021-10-01 20:50:30 -0300

    Alterando

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function App() {
+  const imc = 23;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.app}>
+      <Text style={styles.legenda}>Seu IMC</Text>
+      <View>
+        <Text style={styles.resultado}>{imc}</Text>
+        <Text style={styles.diagnostico}>Normal</Text>
+      </View>
+      <View>
+        <TextInput style={styles.peso} />
+        <TextInput style={styles.altura} />
+        <Button>Calcular</Button>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  app: {
+    fontFamily: "consolas",
+  },
+  legenda: {
+    textAlign: "center",
+    padding: 10,
+    fontWeight: "bold",
+  },
+  resultado: {
+    textAlign: "center",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  diagnostico: {
+    textAlign: "center",
+    fontSize: 16,
+  },
+  peso: {
+    borderColor: "#000",
+    borderWidth: 1,
+  },
+  altura: {
+    borderColor: "#000",
+    borderWidth: 1,
   },
 });

40bcdbf58565d96e7a4e0f0d15b30b797cb93406
jeflucas <jef.lucas@hotmail.co.uk>
2021-10-01 22:52:33 -0300

    Totalmente funcional

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
-import { Button } from "react-native-paper";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, TextInput } from "react-native-paper";
 
 export default class App extends React.Component {
   state = {
@@ -8,6 +8,7 @@ export default class App extends React.Component {
     altura: 0,
     imc: 0,
     legenda: "Indeterminado",
+    cor: "#bdc3c7",
   };
 
   calcularIMC = () => {
@@ -20,22 +21,27 @@ export default class App extends React.Component {
     if (resultado < 18.5) {
       this.setState({
         legenda: "Magreza",
+        cor: "#e74c3c",
       });
     } else if (resultado >= 18.5 && resultado < 25) {
       this.setState({
         legenda: "Normal",
+        cor: "#2ecc71",
       });
     } else if (resultado >= 25 && resultado < 30) {
       this.setState({
         legenda: "Sobrepeso",
+        cor: "#f1c40f",
       });
     } else if (resultado >= 30 && resultado < 40) {
       this.setState({
         legenda: "Obesidade",
+        cor: "#e67e22",
       });
     } else {
       this.setState({
         legenda: "Obesidade Severa",
+        cor: "#e74c3c",
       });
     }
   };
@@ -44,13 +50,13 @@ export default class App extends React.Component {
     return (
       <View style={styles.app}>
         <Text style={styles.legenda}>Seu IMC</Text>
-        <View>
+        <View style={[styles.painel, { backgroundColor: this.state.cor }]}>
           <Text style={styles.resultado}>{this.state.imc}</Text>
           <Text style={styles.diagnostico}>{this.state.legenda}</Text>
         </View>
         <View>
           <TextInput
-            placeholder="Peso"
+            label="Peso"
             keyboardType="number-pad"
             style={styles.peso}
             onChangeText={(valor) => {
@@ -60,7 +66,7 @@ export default class App extends React.Component {
             }}
           />
           <TextInput
-            placeholder="Altura"
+            label="Altura"
             keyboardType="number-pad"
             style={styles.altura}
             onChangeText={(valor) => {
@@ -80,28 +86,43 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   app: {
+    flex: 1,
+    flexDirection: "column",
     fontFamily: "consolas",
+    aligncontent: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  painel: {
+    alignSelf: "center",
+    borderRadius: 15,
+    marginVertical: 10,
+    padding: 8,
+    width: 150,
   },
   legenda: {
     textAlign: "center",
     padding: 10,
     fontWeight: "bold",
+    fontSize: 30,
   },
   resultado: {
+    color: "#fff",
     textAlign: "center",
     fontSize: 22,
     fontWeight: "bold",
   },
   diagnostico: {
+    color: "#fff",
     textAlign: "center",
     fontSize: 16,
   },
   peso: {
-    borderColor: "#000",
+    marginBottom: 10,
     borderWidth: 1,
   },
   altura: {
-    borderColor: "#000",
+    marginBottom: 10,
     borderWidth: 1,
   },
 });

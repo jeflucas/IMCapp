9b7efc75a277ed5e2e7fbea44b0699d3c3637972
jeflucas <jef.lucas@hotmail.co.uk>
2021-10-01 22:01:35 -0300

    Bora

--- App.js
+++ App.js
@@ -2,23 +2,62 @@ import * as React from "react";
 import { StyleSheet, Text, View, TextInput } from "react-native";
 import { Button } from "react-native-paper";
 
-export default function App() {
-  const imc = 23;
+export default class App extends React.Component {
+  state = {
+    peso: 300,
+    altura: 1.88,
+    imc: 0,
+    legenda: "Indeterminado",
+  };
 
-  return (
-    <View style={styles.app}>
-      <Text style={styles.legenda}>Seu IMC</Text>
-      <View>
-        <Text style={styles.resultado}>{imc}</Text>
-        <Text style={styles.diagnostico}>Normal</Text>
+  calcularIMC = () => {
+    const resultado = this.state.peso / this.state.altura ** 2;
+
+    this.setState({
+      imc: resultado.toFixed(2),
+    });
+
+    if (resultado < 18.5) {
+      this.setState({
+        legenda: "Magreza",
+      });
+    } else if (resultado >= 18.5 && resultado < 25) {
+      this.setState({
+        legenda: "Normal",
+      });
+    } else if (resultado >= 25 && resultado < 30) {
+      this.setState({
+        legenda: "Sobrepeso",
+      });
+    } else if (resultado >= 30 && resultado < 40) {
+      this.setState({
+        legenda: "Obesidade",
+      });
+    } else {
+      this.setState({
+        legenda: "Obesidade Severa",
+      });
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.app}>
+        <Text style={styles.legenda}>Seu IMC</Text>
+        <View>
+          <Text style={styles.resultado}>{this.state.imc}</Text>
+          <Text style={styles.diagnostico}>{this.state.legenda}</Text>
+        </View>
+        <View>
+          <TextInput style={styles.peso} />
+          <TextInput style={styles.altura} />
+          <Button mode="contained" onPress={this.calcularIMC}>
+            Calcular
+          </Button>
+        </View>
       </View>
-      <View>
-        <TextInput style={styles.peso} />
-        <TextInput style={styles.altura} />
-        <Button>Calcular</Button>
-      </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
